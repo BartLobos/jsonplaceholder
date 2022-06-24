e20436a63e2ba14fcd09b7d3d41b0a3cb4395d13
Adelance <blobos228@gmail.com>
2022-06-24 09:30:31 +0200

    update id getting

--- src/components/posts/postDetails/newComment/NewComment.tsx
+++ src/components/posts/postDetails/newComment/NewComment.tsx
@@ -1,101 +1,100 @@
-import React, { useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import { useLocation } from 'react-router-dom';
-import { PostContext } from '../../../../context/post/postContext';
-import { locationType } from '../../../../types/Post';
-import './newComment.scss';
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { useLocation } from "react-router-dom";
+import { PostContext } from "../../../../context/post/postContext";
+import { locationType } from "../../../../types/Post";
+import "./newComment.scss";
 
 type Inputs = {
-    email: string;
-    title: string;
-    body: string;
+  email: string;
+  title: string;
+  body: string;
 };
 
 export const NewComment = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset,
-    } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Inputs>();
 
-    let location: locationType = useLocation() as locationType;
-    const id = location.pathname.slice(56, location.pathname.length - 11);
-    const onSubmit = async ({ title, body, email }: Inputs) => {
-        const response = await addComment({
-            postId: parseInt(id),
-            email: email,
-            name: title,
-            body: body,
-        });
-        if (response.status === 201) {
-            alert('Comment added');
-            reset({
-                email: '',
-                title: '',
-                body: '',
-            });
-        } else {
-            alert('Comment failed');
-        }
-    };
-    const postsContext = useContext(PostContext);
+  let location: locationType = useLocation() as locationType;
+  const id = location.pathname.slice(
+    location.pathname.search("posts/") + 6,
+    location.pathname.length - 11
+  );
 
-    const { addComment } = postsContext;
+  const onSubmit = async ({ title, body, email }: Inputs) => {
+    const response = await addComment({
+      postId: parseInt(id),
+      email: email,
+      name: title,
+      body: body,
+    });
+    if (response.status === 201) {
+      alert("Comment added");
+      reset({
+        email: "",
+        title: "",
+        body: "",
+      });
+    } else {
+      alert("Comment failed");
+    }
+  };
+  const postsContext = useContext(PostContext);
 
-    return (
-        <div className='new-comment'>
-            <div className='new-comment--header'>NEW COMMENT</div>
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                className='new-comment--form'>
-                <div className='new-comment--form__group'>
-                    <input
-                        {...register('email', { required: true })}
-                        className='new-comment--form__input'
-                        type='input'
-                        placeholder='Email'
-                        id='email'
-                    />
-                    <label htmlFor='email' className='new-comment--form__label'>
-                        Email
-                    </label>
-                    {errors.email && <span>Email is required</span>}
-                </div>
-                <div className='new-comment--form__group'>
-                    <input
-                        {...register('title', { required: true })}
-                        className='new-comment--form__input'
-                        type='input'
-                        placeholder='Title'
-                        id='title'
-                    />
-                    <label htmlFor='title' className='new-comment--form__label'>
-                        Title
-                    </label>
-                    {errors.title && <span>Title is required</span>}
-                </div>
+  const { addComment } = postsContext;
 
-                <div className='new-comment--form__group'>
-                    <textarea
-                        {...register('body', { required: true })}
-                        className='new-comment--form__input'
-                        placeholder='Body'
-                        id='body'
-                    />
-                    <label htmlFor='body' className='new-comment--form__label'>
-                        Content
-                    </label>
-                    {errors.body && <span>Content is required</span>}
-                </div>
-
-                <div>
-                    <input
-                        type='submit'
-                        className='new-comment--form__button'
-                    />
-                </div>
-            </form>
+  return (
+    <div className="new-comment">
+      <div className="new-comment--header">NEW COMMENT</div>
+      <form onSubmit={handleSubmit(onSubmit)} className="new-comment--form">
+        <div className="new-comment--form__group">
+          <input
+            {...register("email", { required: true })}
+            className="new-comment--form__input"
+            type="input"
+            placeholder="Email"
+            id="email"
+          />
+          <label htmlFor="email" className="new-comment--form__label">
+            Email
+          </label>
+          {errors.email && <span>Email is required</span>}
         </div>
-    );
+        <div className="new-comment--form__group">
+          <input
+            {...register("title", { required: true })}
+            className="new-comment--form__input"
+            type="input"
+            placeholder="Title"
+            id="title"
+          />
+          <label htmlFor="title" className="new-comment--form__label">
+            Title
+          </label>
+          {errors.title && <span>Title is required</span>}
+        </div>
+
+        <div className="new-comment--form__group">
+          <textarea
+            {...register("body", { required: true })}
+            className="new-comment--form__input"
+            placeholder="Body"
+            id="body"
+          />
+          <label htmlFor="body" className="new-comment--form__label">
+            Content
+          </label>
+          {errors.body && <span>Content is required</span>}
+        </div>
+
+        <div>
+          <input type="submit" className="new-comment--form__button" />
+        </div>
+      </form>
+    </div>
+  );
 };
